bdcf7468ec11eef495fd33e4398cfac74968fdcc
RaulCutean <mondekraul1@gmail.com>
2025-04-25 17:40:36 +0300

    Change

--- api/import_script.py
+++ api/import_script.py
@@ -16,11 +16,14 @@ def get_all_recipes():
             ingredients = []
             for ingredient in row['Ingredients']:
                 ingredient_matches = ingredient_regex.match(ingredient)
-                ingredients.append({
-                    'quantity': ingredient_matches['quantity'],
-                    'unit': unit if (unit := ingredient_matches['unit']) else None,
-                    'name': ingredient_matches['name'],
-                })
+                try:
+                    ingredients.append({
+                        'quantity': float(ingredient_matches['quantity']),
+                        'unit': unit if (unit := ingredient_matches['unit']) else None,
+                        'name': ingredient_matches['name'],
+                    })
+                except ValueError as exception:
+                    print(f"Error parsing ingredient: {ingredient}")
             row['Ingredients'] = ingredients
             all_recipes.append(row)
         json.dump(all_recipes, jsonfile)
@@ -30,4 +33,4 @@ def get_all_recipes():
 
 if __name__ == '__main__':
     recipes = get_all_recipes()
-    print(recipes)
+    print(recipe
