d8b7d17020edce93e02578135b4800e46be21dde
RaulCutean <mondekraul1@gmail.com>
2025-04-25 16:34:11 +0300

    -

--- api/models/ingredient.py
+++ api/models/ingredient.py
@@ -6,7 +6,7 @@ class Ingredient(db.Model):
     __tablename__ = "ingredient"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    unit = db.Column(db.String(50), nullable=False)
+    unit = db.Column(db.String(20), nullable=True)
     quantity = db.Column(db.Float, nullable=False)
 
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
